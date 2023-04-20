972d2092c3baefea0cd7244b4f14ea82c058ab56
Andy Short <automation.andy101@gmail.com>
2023-04-20 22:16:40 +0100

    Added ColourList and SingleColour components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import ColourList from './components/ColourList'
+import Form from './components/form'
+import Values from 'values.js';
 
 
 function App() {
 
+  const [colors, setColors] = useState(new Values('#f15025').all(10));  // This gives 10 lighter and 10 darker colours
+  
   return (
-    <></>
+    <main>
+      <Form />
+      <ColourList colors={colors} />
+    </main>
   )
 }
 
---
+++ src/components/ColourList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SingleColour from './SingleColour';
+import { nanoid } from 'nanoid';
+
+
+const ColourList = ({ colors }) => {
+  return (
+    <section className='colors'>
+      {
+        colors.map((color, index) => (
+            <SingleColour key={nanoid()} color={color} index={index} />
+        ))
+      }
+    </section>
+  )
+}
+
+export default ColourList;
