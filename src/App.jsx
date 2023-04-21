9dc9cbe9aeabe9e9b875e7f14265efc5e4748832
Andy Short <automation.andy101@gmail.com>
2023-04-21 08:28:19 +0100

    Added submit form functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import './App.css'
 import ColourList from './components/ColourList'
 import Form from './components/form'
 import Values from 'values.js';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 
 function App() {
 
-  const [colors, setColors] = useState(new Values('#f15025').all(10));  // This gives 10 lighter and 10 darker colours
-  
+  const [colors, setColors] = useState(new Values('#f15025').all(10));  // This increments the values by 10
+
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+      toast.success('Success');
+    } catch(error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor} />
       <ColourList colors={colors} />
+      <ToastContainer position='top-center' />
     </main>
   )
 }
